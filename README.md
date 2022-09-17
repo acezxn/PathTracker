9bf83bdb58f1509b4d5b74b587809360212f092d
acezxn <acezxn@gmail.com>
2022-09-18 01:29:31 +0800

    update documentation

--- README.md
+++ README.md
@@ -13,27 +13,18 @@ modified from [arimb/PurePursuit](https://github.com/arimb/PurePursuit) to be co
 - Used to-scale units (length is in cm)
 - Allowed overlapped paths
 - Added ramsete controller
+- Added simple move and rotate function
+- Added strategy planning system
+- Added get coordinate functionality
 
 ## Functionalities
 <p align="center">
 <img src="https://user-images.githubusercontent.com/14313049/187715396-4e303fdd-b57b-4b49-b3ef-799f9d6185fa.png" width="500" height="500"/>
 </p>
 
-### Path generator
 
-- Because it used Catmull rom spline curve, the first and the last points would not count in the generated path. (You can set the first point to be the same location as the second point, and the last point to be the same location as its previous point.) 
-- Press enter key to confirm path, and other keys to re-plan the path
-- Most functions are largely identicall to [arimb/PurePursuit](https://github.com/arimb/PurePursuit)
 
-### Robot simulator
-
-- Simultes pure pursuit and ramsete algorithm
-- Customized lookahead search algorithm to allow overlapped paths
-- Record the scene and export to png and gif
-
-### Strategy maker
-
-Extends the path generator and allow multiple paths with different tracking algorithms to be planned and combined into one "strategy." The strategies generated would be stored in the strats directory, and simulated with the strategy maker. 
+Code the robot to run multiple paths with different tracking algorithms  and combine into one "strategy." The strategies generated would be stored in the strats directory, and simulated with the strategy maker. 
 
 #### Structures of a strategy folder
 
@@ -41,15 +32,28 @@ Extends the path generator and allow multiple paths with different tracking algo
 ​For example, RAMSETE,1,0 means to use RAMSETE to follow path 1 (the third value has no meaning).
 - ​control_points: A folder that contains user inputted control points of each path, whereas the paths would be ordered numerically.
 - paths: A folder that contains cutmull rom generated paths, which would be numerically ordered.
-- coordinates: A folder containing coordinates of control points in percentage format (0,0 is the top left corder, and 100,100 is the bottom right corner)
+- coordinates: A folder containing coordinates of control points in percentage format (0,0 is the top left corder, and 100,100 is the bottom right corner) for the convenient use of robot programs.
 ​
 ​
-#### paths
+#### Paths
 
 - Yellow straight lines indicates that the robot would follow the path using simply drive forward and turn.
 - Megenta lines indicates that the robot would follow the path with pure pursuit.
 - Blue lines indicates that the robot would follow the path with ramsete.
 
+### Single simulations
+
+#### PathGenerator
+
+- Because it used Catmull rom spline curve, the first and the last points would not count in the generated path. (You can set the first point to be the same location as the second point, and the last point to be the same location as its previous point.) 
+- Press enter key to confirm path, and other keys to re-plan the path
+- Most functions are largely identicall to [arimb/PurePursuit](https://github.com/arimb/PurePursuit)
+
+#### Simulator
+
+- Simultes pure pursuit and ramsete algorithm
+- Customized lookahead search algorithm to allow overlapped paths
+- Record the scene and export to png and gif
 
 ## How to use
 
---
+++ main.py
@@ -0,0 +1,329 @@
+'''
+Make strategies and run them
+'''
+
+from PathGenerator import generate_path
+from objects.Robot import Robot
+import algorithms.PurePursuit as PurePursuit
+import algorithms.Ramsete as Ramsete
+import algorithms.SimpleMoveToPoint as SIMPLE
+
+import os
+import cv2
+import math
+import configparser
+import time
+import imageio
+import tkinter as tk
+from tqdm import tqdm
+import numpy as np
+
+def decrease_brightness(img, value=30):
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(hsv)
+
+        v[True] -= value
+
+        final_hsv = cv2.merge((h, s, v))
+        img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+        return img
+
+strat_name = ""
+mainMenuOption = ""
+pathNo = 0
+
+def makeStrategy():
+    global strat_name
+    
+    window = tk.Tk()
+    window.title("Create strategy")
+    window.geometry("400x200")
+    
+    def set_strat_name():
+        global strat_name
+        strat_name = entry.get()
+        window.destroy()
+
+    def writeToFile():
+        global strat_name, pathNo
+        algo = var.get()
+        actionFile = open(f"strats/{strat_name}/actions.csv", "a+")
+        actionFile.write(f"{algo},{pathNo},0\n")
+        actionFile.close()   
+        print("written")
+        pathNo += 1
+        window.destroy()
+        
+    
+    pos = []
+    label = tk.Label(text="Input the strategy name: ")
+    label.pack()
+    
+    entry = tk.Entry()
+    entry.pack()
+
+    button = tk.Button(window, text= "confirm", height = 2, width = 5, command=set_strat_name)
+    button.pack()
+    
+    window.mainloop()
+    # while strat_name == "":
+    #     pass
+    # strat_name = input("Input the strategy name: ")
+
+    os.mkdir(f"strats/{strat_name}")
+    os.mkdir(f"strats/{strat_name}/control_points")
+    os.mkdir(f"strats/{strat_name}/paths")
+    os.mkdir(f"strats/{strat_name}/coordinates")
+
+    pathNo = 0
+
+    while True:
+        end_pos = generate_path(pos, strat_name)
+        if len(end_pos) == 2:
+            pos = [end_pos[0], -end_pos[1]]
+            
+            window = tk.Tk()
+
+
+            label = tk.Label(text="what algo would you want the robot to follow this path (RAMSETE, PURE_PURSUIT, or SIMPLE): ")
+            label.pack()
+
+            algo_choices = ["SIMPLE", "PURE_PURSUIT", "RAMSETE"]
+            var = tk.StringVar(window)
+            var.set("SIMPLE")
+            selection = tk.OptionMenu(window, var, *algo_choices)
+            selection.pack()
+
+            button = tk.Button(window, text= "confirm", height = 2, width = 5, command=writeToFile)
+            button.pack()
+
+            window.mainloop()
+    
+        else:
+            break
+        
+    
+def runStrategy():
+    global strat_name
+    
+    def set_strat_name():
+        global strat_name
+        strat_name = entry.get()
+        window.destroy()
+    
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    robot = Robot(config)
+
+
+    field_length = float(config["FIELD_IMAGE"]["FIELD_LENGTH"])
+    img_dimension = float(config["FIELD_IMAGE"]["IMAGE_LENGTH"])
+    export_enabled = int(config["EXPORT"]["ENABLED"])
+    imageNames = []
+
+    scaler = field_length / img_dimension
+
+    # parse actions.csv
+    window = tk.Tk()
+    window.title("Run strategy")
+    window.geometry("400x200")
+    
+    label = tk.Label(text="Input the strategy name: ")
+    label.pack()
+    
+    entry = tk.Entry()
+    entry.pack()
+
+    button = tk.Button(window, text= "confirm", height = 2, width = 5, command=set_strat_name)
+    button.pack()
+    
+    window.mainloop()
+    
+    strat_dir = f"strats/{strat_name}/"
+    actionFile = open(strat_dir + "actions.csv", "r")
+    actions = []
+    for line in actionFile.readlines():
+        inner = []
+        for l in line.split(","):
+            if l.strip().isdigit():
+                inner.append(int(l))
+            else:
+                inner.append(l)
+        actions.append(inner)
+    print(actions)
+    
+    # field = cv2.imread(config["FIELD_IMAGE"]["FILE_LOCATION"])
+    # img = decrease_brightness(field, 100)
+    
+    # cv2.imshow("img", img)
+    
+    # cv2.waitKey(0) 
+    
+    # run actions.csv
+    for k, command in enumerate(actions):
+        if command[0] == "SIMPLE":
+            path = []
+            pathNo = command[1]
+            pathFile = open(strat_dir + f"control_points/{pathNo}.csv", "r")
+            for line in pathFile.readlines():
+                inner = []
+                for i in range(len(line.split(","))):
+                    if i == 1:
+                        inner.append(-float(line.split(",")[i])/scaler)
+                    else:
+                        inner.append(float(line.split(",")[i])/scaler)
+                path.append(list(inner))
+            robot.start_pos = path[0]
+            print("turning", robot.angle)
+            SIMPLE.run(robot, strat_name, [path[2],path[2]], True) 
+            # SIMPLE.rotateToAngle(path[0], math.atan2(path[2][0], path[2][1]))
+            
+            SIMPLE.run(robot, strat_name, path, False)   
+            if (k == len(actions)-1):    
+                key = cv2.waitKey()
+                    
+
+        elif command[0] == "PURE_PURSUIT":
+            path = []
+            pathNo = command[1]
+            pathFile = open(strat_dir + f"paths/{pathNo}.csv", "r")
+            for line in pathFile.readlines():
+                inner = []
+                for i in range(len(line.split(","))):
+                    if i == 1:
+                        inner.append(-float(line.split(",")[i])/scaler)
+                    else:
+                        inner.append(float(line.split(",")[i])/scaler)
+                path.append(list(inner))
+            # SIMPLE.rotateToAngle(path[0], math.atan2(path[2][0], path[2][1]))
+            robot.start_pos = path[0]
+            SIMPLE.run(robot, strat_name, [path[2],path[2]], True) 
+
+            PurePursuit.run(robot, strat_name, path)        
+            if (k == len(actions)-1):    
+                key = cv2.waitKey()
+
+        elif command[0] == "RAMSETE":
+            path = []
+            pathNo = command[1]
+            pathFile = open(strat_dir + f"paths/{pathNo}.csv", "r")
+            for line in pathFile.readlines():
+                inner = []
+                for i in range(len(line.split(","))):
+                    if i == 1:
+                        inner.append(-float(line.split(",")[i])/scaler)
+                    else:
+                        inner.append(float(line.split(",")[i])/scaler)
+                path.append(list(inner))
+            # SIMPLE.rotateToAngle(path[0], math.atan2(path[2][0], path[2][1]))
+            robot.start_pos = path[0]
+            print("turning", robot.angle)
+            SIMPLE.run(robot, strat_name, [path[2],path[2]], True) 
+
+            Ramsete.run(robot, strat_name, path)     
+            if (k == len(actions)-1):    
+                key = cv2.waitKey()   
+    
+    if export_enabled:
+        with imageio.get_writer('images/movie.gif', mode='I') as writer:
+            print("Writing gif to images/movie.gif")
+            for name in tqdm(robot.imageNames):
+                writer.append_data(imageio.imread(name))
+            print("Done!")
+
+def mainMenuSelect():
+        global mainMenuOption
+        mainMenuOption = var.get()
+        window.destroy()
+
+def draw_robot(img, x, y):
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    
+    field_length = float(config["FIELD_IMAGE"]["FIELD_LENGTH"])
+    img_dimension = float(config["FIELD_IMAGE"]["IMAGE_LENGTH"])
+
+    scaler = field_length / img_dimension # scaler for cm conversion
+    length = float(config["ROBOT"]["LENGTH"]) / scaler
+    width = float(config["ROBOT"]["TRACKWIDTH"]) / scaler
+    
+    tmp = img.copy()
+    cv2.circle(tmp, (int(x), int(y)), 4,
+            (0, 255, 255), -1)
+    cv2.drawContours(tmp, [np.array([((x+length/2*math.sin(0)-width/2*math.cos(0)),
+                                    (y+length/2*math.cos(0)+width/2*math.sin(0))),
+                                    ((x+length/2*math.sin(0)+width/2*math.cos(0)),
+                                    (y+length/2*math.cos(0)-width/2*math.sin(0))),
+                                    ((x-length/2*math.sin(0)+width/2*math.cos(0)),
+                                    (y-length/2*math.cos(0)-width/2*math.sin(0))),
+                                    ((x-length/2*math.sin(0)-width/2*math.cos(0)),
+                                    (y-length/2*math.cos(0)+width/2*math.sin(0)))])
+                    .reshape((-1,1,2)).astype(np.int32)], 0, (0, 255, 255), 2)
+    cv2.circle(tmp, (int(x), int(y)), int(math.sqrt((length/2)**2 + (width/2)**2)), (0, 255, 0), 1)
+    cv2.imshow("img", tmp)
+    
+def findCoordinate():
+    global mouse_down, scaler, img
+    
+    
+    def click(event, x, y, flags, param):
+        global mouse_down, scaler, img
+        draw_robot(img, x, y)
+        if event == cv2.EVENT_LBUTTONDOWN:
+            mouse_down = True
+        if event == cv2.EVENT_LBUTTONUP and mouse_down:
+            mouse_down = False
+            window = tk.Tk()
+            label = tk.Label(text=f"({x * scaler}, {y * scaler})")
+            label.pack()
+            window.mainloop()
+            
+            
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    
+    field = cv2.imread(config["FIELD_IMAGE"]["FILE_LOCATION"])
+    img_dimension = float(config["FIELD_IMAGE"]["IMAGE_LENGTH"])
+    export_enabled = int(config["EXPORT"]["ENABLED"])
+    imageNames = []
+
+    scaler = 100 / img_dimension
+    
+    img = decrease_brightness(field, 100)
+    mouse_down = False
+    
+    cv2.imshow("img", img)
+    cv2.setMouseCallback('img', click)
+    
+    cv2.waitKey(0) 
+    
+
+
+if __name__ == "__main__":
+    window = tk.Tk()
+    window.title("Main menu")
+    window.geometry("400x400")
+    
+    label = tk.Label(text="PathTracker", font = ("Arial", 30))
+    label.pack(pady = 100)
+    
+    options = ["Create a new strategy", "Run a strategy", "Find coordinates"]
+    var = tk.StringVar(window)
+    var.set("Create a new strategy")
+    selection = tk.OptionMenu(window, var, *options)
+    selection.config(width=len(max(options, key=len)))
+    selection.pack(pady = 10, ipadx=10)
+    
+    button = tk.Button(window, text= "confirm", height = 2, width = 5, command=mainMenuSelect)
+    button.pack()
+    
+    window.mainloop()
+
+    if (mainMenuOption == options[0]):
+        makeStrategy()
+    elif (mainMenuOption == options[1]):
+        runStrategy()
+    elif (mainMenuOption == options[2]):
+        findCoordinate()
+    
